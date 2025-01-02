085f5b2ea0092f7512d740289ad217a40d42ff9b
KatUser <ekaterina.shirokikh@gmail.com>
2025-01-03 06:40:16 +0700

    fix getter

--- src/main/java/ru/job4j/todo/model/Task.java
+++ src/main/java/ru/job4j/todo/model/Task.java
@@ -1,39 +1,26 @@
 package ru.job4j.todo.model;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
 @Getter
+@Setter
 @Entity
 @Table(name = "task")
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@AllArgsConstructor
+@NoArgsConstructor
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @EqualsAndHashCode.Exclude
+    @EqualsAndHashCode.Include
     private Integer id;
     private String title;
     private String description;
     private LocalDateTime created
             = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
     private boolean done;
-
-    public Task(int id,
-                String title,
-                String description,
-                LocalDateTime created,
-                boolean done) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.created = created;
-        this.done = done;
-    }
-
-    public Task() {
-    }
 }
