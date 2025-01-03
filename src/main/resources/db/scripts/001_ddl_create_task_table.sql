1a9d4e564ec910665b6324ead06bad5f45a6eec2
KatUser <ekaterina.shirokikh@gmail.com>
2025-01-04 06:36:48 +0700

    title TEXT not null

--- src/main/resources/db/scripts/001_ddl_create_task_table.sql
+++ src/main/resources/db/scripts/001_ddl_create_task_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE task
 (
     id          SERIAL PRIMARY KEY,
-    title       VARCHAR(32) unique not null,
-    description TEXT                not null,
+    title       TEXT not null,
+    description TEXT not null,
     created     TIMESTAMP,
     done        BOOLEAN
 );
