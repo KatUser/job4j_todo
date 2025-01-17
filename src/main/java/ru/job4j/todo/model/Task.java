9cc6eb3d53e4e970374ef1f62290df0a1fcaafcd
KatUser <ekaterina.shirokikh@gmail.com>
2025-01-17 17:54:09 +0700

    3. LazyInitializationexception [#331987]

--- src/main/java/ru/job4j/todo/model/Task.java
+++ src/main/java/ru/job4j/todo/model/Task.java
@@ -27,4 +27,8 @@ public class Task {
     @ManyToOne
     @JoinColumn(name = "todo_user_id")
     private User user;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name = "priority_id")
+    private Priority priority;
 }
